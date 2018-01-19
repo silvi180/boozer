9b80067a72634a9727f1a1f1554916fb94966ed0
dansbands <dansbands@hotmail.com>
2018-01-19 11:13:41 -0500

    htmlFor

--- src/CocktailForm.js
+++ src/CocktailForm.js
@@ -7,32 +7,32 @@ const CocktailForm = (props) => {
       <h3>Cocktail Form</h3>
         <form action="/action_page.php">
           <div className="form-group">
-            <label for="name">Name:</label>
+            <label htmlFor="name">Name:</label>
             <input type="text" className="form-control" id="name"/>
           </div>
 
           <div className="form-group">
-            <label for="description">Description:</label>
+            <label htmlFor="description">Description:</label>
             <textarea type="text" className="form-control" id="description"/>
           </div>
 
           <div className="form-group">
-            <label for="instructions">Instructions:</label>
+            <label htmlFor="instructions">Instructions:</label>
             <textarea type="text" className="form-control" id="instructions"/>
           </div>
 
           <div className="form-group">
-            <label for="source">Source:</label>
+            <label htmlFor="source">Source:</label>
             <input type="text" className="form-control" id="source"/>
           </div>
           <h4>Proportions</h4>
           <div className="row">
             <div className="col-xs-6">
-              <label for="ingredient">Ingredient:</label>
+              <label htmlFor="ingredient">Ingredient:</label>
               <input type="text" className="form-control" id="ingredient"/>
             </div>
             <div className="col-xs-6">
-              <label for="ingredient">Quantity:</label>
+              <label htmlFor="ingredient">Quantity:</label>
               <input type="text" className="form-control" id="ingredient"/>
             </div>
           </div>
