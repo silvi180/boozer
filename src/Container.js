e5fb9564fdf7e294756845391b27e90258c519e5
Silvia Chung <schung97@gmail.com>
2018-03-14 18:45:08 -0400

    file change

--- src/Container.js
+++ src/Container.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import './css/App.css';
+import './css/app.css';
 import CocktailsContainer from './CocktailsContainer';
 import MainContent from './MainContent';
 import SearchBar from './SearchBar';
