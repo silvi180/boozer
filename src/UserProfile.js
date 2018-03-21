eb81131e3c3644776e9c9634916e112377f34027
Silvia Chung <schung97@gmail.com>
2018-03-21 02:49:33 -0400

    saved button edit

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -48,7 +48,7 @@ const UserProfile = (props) => {
       return(
         props.user ?
           <div className="col-md-4">
-            <img src="./img/pexels-photo-616834.jpg" alt="default pic"/>
+            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="default pic" style={{width: '450px'}}/>
             <h3>{props.user.firstname} {props.user.lastname}</h3>
             <h5>About</h5>
             <p>{props.user.bio}</p>
